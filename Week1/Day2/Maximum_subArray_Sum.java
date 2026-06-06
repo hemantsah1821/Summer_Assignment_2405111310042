5bda89942ef76b7502ecdb6040ec3b5796704bee
hemantSah1821 <hemant949079@gmail.com>
2026-06-07 00:13:58 +0530

    update Maximum subArray Sum

---
+++ Week1/Day2/Maximum_subArray_Sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+            int maxSum = Integer.MIN_VALUE;
+            int currSum = 0;
+            int prefix[] = new int[nums.length];
+
+            //calculate prefix
+            prefix[0] = nums[0];
+            for(int i =1; i<prefix.length; i++){
+                prefix[i] = prefix[i-1] + nums[i];
+            }
+            for(int i=0; i<nums.length; i++){
+                int start = i;
+                for(int j=i; j<nums.length; j++){
+                    int end = j;
+                    currSum = start == 0 ? prefix[end] : prefix[start-1];
+                    if(maxSum < currSum){
+                       maxSum = currSum;
+                    }
+        }     
+    }
+        return maxSum;
+    }
+   }
